5a5e0e3bbb112c98e050ebd49c7a2a4138bdebbb
Fatemeh <fatemehghasemi9900@gmail.com>
2023-01-16 11:32:48 +0330

    Display the list of recruitments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 console.clear();
-// console.log("hello love ;)");
 const BASE_URL = "https://www.arbeitnow.com/api/job-board-api";
 const container = document.getElementById("container");
+let data = [];
 
 const getCard = async (url) => {
   try {
     const response = await fetch(url);
-    const data = response.json();
+    const dataArray = await response.json();
+    data = dataArray.data;
     console.log(data);
-    console.log(data.links);
-    // makeCardEmployment(data);
+    makeCardEmployment(data);
   } catch (err) {
     console.log("error: ", err);
   }
@@ -18,8 +18,15 @@ const getCard = async (url) => {
 const makeCardEmployment = (data) => {
   let cardEmployment = "";
   data.map((d) => {
-    // console.log(d);
-    cardEmployment += `<article><h3>${d.company_name}</h3></article>`;
+    cardEmployment += `
+      <article class="card w-75 mb-3 shadow">
+          <div class="card-body">
+            <h5 class="card-title d-inline-block">${d.title}</h5>
+            <a href="#" class="card-link btn btn-primary float-end">more</a>
+            <h6 class="card-subtitle mb-2 text-muted">${d.company_name}</h6>
+            <p class="card-text">${d.slug}</p>
+          </div>
+      </article>`;
   });
   container.innerHTML = cardEmployment;
 };
